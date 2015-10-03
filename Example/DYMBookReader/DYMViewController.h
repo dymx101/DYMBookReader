a7530381761ff6c6c810c113dad3b1bd1147d7d2
Daniel Dong <dymx101@hotmail.com>
2015-10-03 16:59:26 +0800

    initial commit

---
+++ Example/DYMBookReader/DYMViewController.h
@@ -0,0 +1,13 @@
+//
+//  DYMViewController.h
+//  DYMBookReader
+//
+//  Created by Daniel Dong on 10/03/2015.
+//  Copyright (c) 2015 Daniel Dong. All rights reserved.
+//
+
+@import UIKit;
+
+@interface DYMViewController : UIViewController
+
+@end
---
+++ Example/Pods/Target Support Files/DYMBookReader/DYMBookReader-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double DYMBookReaderVersionNumber;
+FOUNDATION_EXPORT const unsigned char DYMBookReaderVersionString[];
+
